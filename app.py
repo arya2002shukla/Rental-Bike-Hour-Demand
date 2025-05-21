e62ee42c7935f17377deac5fc47def0a2335e8ac
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2025-05-21 19:38:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sn
+import matplotlib
+matplotlib.use('Agg')  # Use non-GUI backend suitable for servers
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
